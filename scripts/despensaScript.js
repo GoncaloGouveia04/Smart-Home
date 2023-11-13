968e72827a15d4edb04749d928f90305c64744cc
Gonçalo Gouveia <109439282+GoncaloGouveia04@users.noreply.github.com>
2023-11-13 16:10:08 +0000

    Add files via upload

--- scripts/despensaScript.js
+++ scripts/despensaScript.js
@@ -10,38 +10,125 @@ const DESPENSA_DIV = "despensaArea";
 
 const OPTIONS_BUTTON = "optionsBtn";
 
+const CLOSE_OPTIONS_WINDOW_BTN = "closeBtn";
+
+const popupDiv = document.getElementById("definicoes-window");
+
+const ecraDiv = document.getElementById("ecra");
+
+const productsList = ["Barra de Chocolate", "Cereais", "Iogurte Natural", "Maçã", "Azeite",
+    "Pacote de Bolacha", "Leite", "Leite Condensado", "Lata de Atum", "Posta de Salmão",
+    "Cenoura", "Tomate", "Pacote de Arroz", "Pacote de Massa", "Batata",
+    "Lombo de Porco", "Peito de Frango", "Picanha", "Garrafa de Água", "Pacote de açúcar",
+    "Pacote de Sal", "Conserva de Feijão", "Conserva de Grão", "Sacos de Café", "Doce de Morango"]
+
 window.addEventListener("load", principal);
 
-const produtos = ["imagens/apple.png", "imagens/leite.png", "imagens/chocapic.png"];
-
-function principal(){
-
-    addProducts();
+function createProduct(name) {
+    this.name = name;
+    this.quantity = Math.floor(Math.random());
+    this.expireDate = "a";
+}
+function principal() {
+    defineEventHandlers();
+    //addProducts();
+    getAlarms();
 }
 
-function defineEventHandlers(){
+function defineEventHandlers() {
+    document.getElementById(OPTIONS_BUTTON).addEventListener("click", showWindow);
+    document.getElementById(CLOSE_OPTIONS_WINDOW_BTN).addEventListener("click", closeWindow);
+    defineAlarms();
+    defineOrderOfProducts();
+    showProdutsOption();
+}
+
+
+
+function displayBoardHTML() {
+    let html = '<table class="produtosDespensa">';
+    let i = 0
+    for (let row = 0; row < 5; row++) {
+        html += "<tr>";
+
+        for (let column = 0; column < 5; column++) {
+            let product = productsList[i]
+            html += "<td class='game' id='td_" + getCellID(row, jewel) + "'>\
+            <img class='tablepiece' id='" + getCellID(row, jewel) + "' src=../media/" + jewel_type + "_jewel.png></td>";
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+    return html;
+}
+
+
+function comprarProduto() {
 
 }
 
-function addProducts(){
-    
-    for(let x = 0;x < 25; x++){
-        const novoProduto = document.createElement("div");
-        const imagemProduto = document.createElement("img");
-        const indexAleatorio = Math.floor(Math.random() * produtos.length);
+function showWindow() {
+    popupDiv.style.display = "block";
+}
 
-        imagemProduto.width = "100";
-        imagemProduto.height = "100";
-        imagemProduto.src = produtos[indexAleatorio];
-        novoProduto.classList.add("produto");
-        novoProduto.addEventListener("click", comprarProduto);
-        novoProduto.appendChild(imagemProduto);
-        document.getElementById(DESPENSA_DIV).appendChild(novoProduto);
+function closeWindow() {
+    popupDiv.style.display = "none";
+}
 
+function defineAlarms() {
+    document.getElementById("validade").children[0].addEventListener("click", function () {
+        document.getElementById("validade").children[0].disabled = true;
+        document.getElementById("validade").children[1].disabled = false;
+        localStorage.setItem("Alerta de data de validade", "ON");
+    });
 
+    document.getElementById("validade").children[1].addEventListener("click", function () {
+        document.getElementById("validade").children[1].disabled = true;
+        document.getElementById("validade").children[0].disabled = false;
+        localStorage.setItem("Alerta de data de validade", "OFF");
+
+    });
+
+    document.getElementById("falta").children[0].addEventListener("click", function () {
+        document.getElementById("falta").children[0].disabled = true;
+        document.getElementById("falta").children[1].disabled = false;
+        localStorage.setItem("Alerta de falta de produtos", "ON");
+    });
+
+    document.getElementById("falta").children[1].addEventListener("click", function () {
+        document.getElementById("falta").children[1].disabled = true;
+        document.getElementById("falta").children[0].disabled = false;
+        localStorage.setItem("Alerta de falta de produtos", "OFF");
+    });
+}
+
+function getAlarms() {
+    if (localStorage.getItem("Alerta de data de validade") == "ON") {
+        document.getElementById("validade").children[0].disabled = true;
+        document.getElementById("validade").children[1].disabled = false;
+    } else {
+        document.getElementById("validade").children[0].disabled = false;
+        document.getElementById("validade").children[1].disabled = true;
+    }
+
+    if (localStorage.getItem("Alerta de falta de produtos") == "ON") {
+        document.getElementById("falta").children[0].disabled = true;
+        document.getElementById("falta").children[1].disabled = false;
+    } else {
+        document.getElementById("falta").children[0].disabled = false;
+        document.getElementById("falta").children[1].disabled = true;
     }
 }
 
-function comprarProduto(){
-    
+function defineOrderOfProducts(){
+    console.log(document.getElementById("quantidade"));
+    if(document.getElementById("quantidade").checked){
+        localStorage.setItem("Ordenar produtos por: ", "Quantidade");
+    }else{
+        localStorage.setItem("Ordenar produtos por: ", "Validade");
+    }
+}
+
+function showProdutsOption(){
+
 }
